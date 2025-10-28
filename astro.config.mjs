6f7668a77bd3b52be03dc723866bab343baa6a9e
Eduardo <esanchez@gmx.net>
2025-10-28 15:24:39 -0600

    First initial commit for my AWSAstroApp

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import awsAmplify from 'astro-aws-amplify';
+
+// https://astro.build/config
+export default defineConfig({
+
+	output: "server",
+	adapter: awsAmplify(),
+});
